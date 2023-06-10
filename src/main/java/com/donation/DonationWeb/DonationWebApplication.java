8dc5ae271d9a7fed2e0120544d0d3cb509aabf86
whdcks2252 <whdcks4569@naver.com>
2023-06-10 17:56:46 +0900

    test1

---
+++ src/main/java/com/donation/DonationWeb/DonationWebApplication.java
@@ -0,0 +1,13 @@
+package com.donation.DonationWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationWebApplication.class, args);
+	}
+	///gdgdgdsdafsdafasdfsad
+}
---
+++ src/test/java/com/donation/DonationWeb/DonationWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donation.DonationWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
